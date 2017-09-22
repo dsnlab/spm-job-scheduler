5268cf3796ff0a7210a994800b1e783b37760e9c
katemills <klmills@uoregon.edu>
2017-09-22 13:04:37 -0700

    check arguments

--- batch_arbitrary_matlab_script.sh
+++ batch_arbitrary_matlab_script.sh
@@ -14,8 +14,14 @@
 
 ADDITIONALOPTIONS="-singleCompThread"
 
+if [ -z "$1" ]
+  then
+    echo "No argument supplied. Must give matlab script to be scheduled as first argument"
+    exit 1
+fi
+
 SCRIPT=`realpath $1`
 
 echo Running $SCRIPT
 module load matlab
-srun matlab -nosplash -nodisplay -nodesktop ${ADDITIONALOPTIONS} -r "run('$SCRIPT')" 
+srun matlab -nosplash -nodisplay -nodesktop ${ADDITIONALOPTIONS} -r "run('$SCRIPT');exit" 
